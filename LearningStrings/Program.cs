d1f5ce0b4e18a9b150b66b07b93d12845a9fdf6f
mag1987 <mag987@list.ru>
2018-11-11 19:59:30 +0700

    поиск общего слова не чувствителен к регистру, проблема нулевой строки (точка в конце предложения генерирует пустую строку)

--- LearningStrings/Program.cs
+++ LearningStrings/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LearningStrings
@@ -12,8 +13,8 @@ namespace LearningStrings
         {
             string outString;
 
-            string stringPart1 = "Жили-были дед, да баба. Ели";
-            string stringPart2 = " кашу с молоком, особенно дед. дед ";
+            string stringPart1 = "Жили-были дед, да баба? Ели";
+            string stringPart2 = " кашу с молоком, особенно дед. Дед ";
             string stringPart3 = "на бабу рассердился и пошел кодить на сях.";
 
             StringBuilder tmp = new StringBuilder();
@@ -23,26 +24,40 @@ namespace LearningStrings
 
             Console.WriteLine(tmp);
 
-            string[] sentences = tmp.ToString().Split('.');
+            string[] sentences = Regex.Split(tmp.ToString(), @"[\.!\?]+");
+            
+            for (int i =0; i< sentences.Length; i++)
+            {
+                sentences[i] = Regex.Replace(sentences[i], @"[,;:]*", "");
+            }
+            
+            Console.WriteLine(sentences[0]);
+            Console.WriteLine(sentences[1]);
+            Console.WriteLine(sentences[2]);
 
-            Console.WriteLine(CommonWord(sentences[0], sentences[2]));
+            Console.WriteLine(CommonWord(sentences[0],sentences[2]));
 
-            //string[] words = sentences[1].Split(' ');
-           
+            Console.WriteLine(sentences.Length);
+            Console.WriteLine(String.Compare("половник","пол",true));
+            Console.WriteLine(String.Compare("пол", "половина", true));
+            Console.WriteLine(String.Compare("пОл", "пол", true));
+            Console.WriteLine(String.Compare("пол", "пол", true));
+            
+
 
         }
 
         static string CommonWord(string a, string b)
         {
-            string[] wordsA = a.Split(' ');
-            string[] wordsB = b.Split(' ');
+            string[] wordsA = Regex.Replace(a, @"[,;:]*", "").Split(' ');
+            string[] wordsB = Regex.Replace(b, @"[,;:]*", "").Split(' ');
             string word;
             for (int i = 0; i< wordsA.Length; i++)
             {
                 word = wordsA[i];
                 for (int j =0; j<wordsB.Length; j++)
                 {
-                    if (word == wordsB[j])
+                    if (String.Compare(word, wordsB[j], true) == 0)
                         return word;
                 }
             }
@@ -59,7 +74,12 @@ namespace LearningStrings
             {
                 word = CommonWord(word, sentences[i]);
             }
-            return word;
+            
+            if (word == null)
+                return "Общего слова не найдено";
+            else
+            
+                return word;
         }
     }
 }
