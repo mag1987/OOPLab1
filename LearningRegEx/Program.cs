bc41ef02060aaa0b2a6f250c662c751690fbadb0
mag1987 <mag987@list.ru>
2018-11-17 19:57:02 +0700

    поиск цифр в строке

---
+++ LearningRegEx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LearningRegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Если эта проблема возникла раньше, позвоните по телефону 896 товарищам разработчикам.";
+            string s2 = "Если эта проблема возникла раньше, позвоните по телефону (не знаю какому) товарищам разработчикам.";
+
+            Console.WriteLine("{0} {1}", s1, hasDigital(s1));
+            Console.WriteLine("{0} {1}", s2, hasDigital(s2));
+        }
+        static string hasDigital(string s)
+        {
+            return Regex.IsMatch(s, @"\d") ? 
+                "В строке содержатся цифры"
+                :"В строке нет цифр";
+        }
+    }
+}
