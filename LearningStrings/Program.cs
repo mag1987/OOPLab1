37b61504fc298a3155be91f10eb8a390600cd10d
mag1987 <mag987@list.ru>
2018-11-17 16:54:06 +0700

    Более правильный вариант с регулярными выражениями, нерабочий

--- LearningStrings/Program.cs
+++ LearningStrings/Program.cs
@@ -14,7 +14,7 @@ namespace LearningStrings
             string outString;
 
             string stringPart1 = "Жили-были дед, да баба? Ели";
-            string stringPart2 = " кашу с молоком, особенно дед. Дед ";
+            string stringPart2 = " кашу с молоком, особенно дед.   Дед ";
             string stringPart3 = "на бабу рассердился и пошел кодить на сях.";
 
             StringBuilder tmp = new StringBuilder();
@@ -24,32 +24,71 @@ namespace LearningStrings
 
             Console.WriteLine(tmp);
 
+            Regex sentencePattern = new Regex(@"[^\.!\?]+[\.!\?]+");
+            Regex wordPattern = new Regex(@"\w+-?\w*\b", RegexOptions.IgnoreCase);
+           
+            Match sentence = sentencePattern.Match(tmp.ToString());
+            Match word = wordPattern.Match(sentence.ToString());
+            Match commonWord = word;
+
+            Match nextSentence = sentence.NextMatch();
+            Console.WriteLine(sentence.ToString());
+            while (sentence.Success && word.Success)
+            {
+                sentence = sentence.NextMatch();
+              
+
+
+                Console.WriteLine("{0} {1}", 1, sentence.ToString());
+                 Console.WriteLine("{0} {1}", 2, nextSentence.ToString());
+                
+               
+                
+                nextSentence = nextSentence.NextMatch();
+            }
+            /*
             string[] sentences = Regex.Split(tmp.ToString(), @"[\.!\?]+");
             
             for (int i =0; i< sentences.Length; i++)
             {
                 sentences[i] = Regex.Replace(sentences[i], @"[,;:]*", "");
             }
-            
+            MatchCollection[] wordsInSentences = new MatchCollection[sentences.Length];
+            Regex wordPattern = new Regex(@"\w+-?\w*\b");
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                wordsInSentences[i] = wordPattern.Matches(sentences[i]);
+                foreach (Match t in wordsInSentences[i])
+                {
+                    Console.WriteLine("{0} {1}", t.ToString(), wordsInSentences[i].Count);
+                    wordPattern.Match(sentences[i]);
+                    
+                }
+            }
+            */
+            /*
             Console.WriteLine(sentences[0]);
             Console.WriteLine(sentences[1]);
             Console.WriteLine(sentences[2]);
+            Console.WriteLine(sentences[3]);
+           
 
-            Console.WriteLine(CommonWord(sentences[0],sentences[2]));
+            Console.WriteLine(CommonWord(sentences));
 
             Console.WriteLine(sentences.Length);
             Console.WriteLine(String.Compare("половник","пол",true));
             Console.WriteLine(String.Compare("пол", "половина", true));
             Console.WriteLine(String.Compare("пОл", "пол", true));
             Console.WriteLine(String.Compare("пол", "пол", true));
+            */
             
-
 
         }
 
         static string CommonWord(string a, string b)
         {
             string[] wordsA = Regex.Replace(a, @"[,;:]*", "").Split(' ');
+            //Regex.Matches()
             string[] wordsB = Regex.Replace(b, @"[,;:]*", "").Split(' ');
             string word;
             for (int i = 0; i< wordsA.Length; i++)
@@ -63,6 +102,28 @@ namespace LearningStrings
             }
             return null;
         }
+        /*
+        static string CommonWord2(string a, string b)
+        {
+            Regex wordPattern = new Regex(@"\w+-?\w*\b");
+            MatchCollection wordsA = ;
+            
+            string[] wordsA = Regex.Replace(a, @"[,;:]*", "").Split(' ');
+            //Regex.Matches()
+            string[] wordsB = Regex.Replace(b, @"[,;:]*", "").Split(' ');
+            string word;
+            for (int i = 0; i < wordsA.Length; i++)
+            {
+                word = wordsA[i];
+                for (int j = 0; j < wordsB.Length; j++)
+                {
+                    if (String.Compare(word, wordsB[j], true) == 0)
+                        return word;
+                }
+            }
+            return null;
+        }
+        */
         static string CommonWord(string[] sentences)
         {
             string word = null;
